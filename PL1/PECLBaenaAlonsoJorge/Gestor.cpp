86472a4065672c6c4b3f5a27c89915652c83fcc0
AndreiFarcas <fandrei800@gmail.com>
2025-10-24 10:59:01 +0200

    Add files via upload

--- PL1/PECLBaenaAlonsoJorge/Gestor.cpp
+++ PL1/PECLBaenaAlonsoJorge/Gestor.cpp
@@ -1,32 +1,58 @@
-#include <iostream>
 #include "Gestor.hpp"
 #include "Aficionados.hpp"
+#include <iostream>
 using namespace std;
 
-Gestor::Gestor()
-{
-	ronda = 0;
+Gestor::Gestor() { ronda = 0; }
+
+void Gestor::genera10Aficionados() {
+  int n = 10;
+  int a[n];
+  cout << "Id ordenadas: " << endl;
+  for (int i = 0; i < n; i++) {
+    a[i] = i + 10 * ronda;
+    Aficionado *aficionado = new Aficionado(a[i]);
+    pilaAficionado.insertar(aficionado);
+    cout << a[i] << " ";
+  }
+  ronda++;
 }
 
-void Gestor::genera10Aficionados(){
-	int n = 10;
-	int a[n];
-	cout << "Id ordenadas: " << endl;
-	for(int i=0; i<n; i++){
-		a[i] = i+10*ronda;
-		Aficionado*aficionado = new Aficionado(a[i]);
-		pilaAficionado.insertar(aficionado);
-		cout << a[i] << " ";
-	}
-	ronda++;
+void Gestor::muestraAficionados() { pilaAficionado.mostrar(); }
 
+void Gestor::borraAficionadosPila() {
+  cout << "Borrando todos los aficionados de la pila..." << endl;
+
+  while (pilaAficionado.getLongitud() > 0) {
+    Aficionado *af =
+        pilaAficionado.extraer(); // extrae el nodo y devuelve el aficionado
+    delete af;                    // libera la memoria del aficionado
+  }
+
+  cout << "Pila vaciada correctamente." << endl;
+}
+void Gestor::encolarAficionados() {
+  if (pilaAficionado.getLongitud() == 0) {
+    cout << "No hay aficionados en la pila para encolar." << endl;
+    return;
+  }
+
+  cout << "Encolando aficionados..." << endl;
+
+  while (pilaAficionado.getLongitud() > 0) {
+    Aficionado *af = pilaAficionado.extraer(); // Sacamos de la pila
+
+    if (af->getEsSocio()) {
+      colaSocios.insertar(af);
+      cout << "Aficionado ID:" << af->getId() << " (SOCIO) encolado." << endl;
+    } else {
+      colaSimpatizantes.insertar(af);
+      cout << "Aficionado ID:" << af->getId() << " (SIMPATIZANTE) encolado."
+           << endl;
+    }
+  }
+
+  cout << "Todos los aficionados fueron encolados correctamente." << endl;
 }
 
-void Gestor::muestraAficionados(){
-	pilaAficionado.mostrar();
-	}
-
-Gestor::~Gestor()
-{
-}
-
+Gestor::~Gestor() {}
--- PL1/PECLBaenaAlonsoJorge/PECLBaenaAlonsoJorge.mk
+++ PL1/PECLBaenaAlonsoJorge/PECLBaenaAlonsoJorge.mk
@@ -6,16 +6,16 @@
 ProjectName            :=PECLBaenaAlonsoJorge
 ConfigurationName      :=Debug
 WorkspaceConfiguration :=Debug
-WorkspacePath          :=C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1
-ProjectPath            :=C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge
+WorkspacePath          :=C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1
+ProjectPath            :=C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge
 IntermediateDirectory  :=../build-$(WorkspaceConfiguration)/PECLBaenaAlonsoJorge
 OutDir                 :=$(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=rosaa
+User                   :=lolpi
 Date                   :=10/24/2025
-CodeLitePath           :=C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite
+CodeLitePath           :=C:/codelite-amd64-17.0.0/codelite
 MakeDirCommand         :=mkdir
 LinkerName             :=g++
 SharedObjectLinkerName :=g++ -shared -fPIC
@@ -29,7 +29,7 @@ OutputSwitch           :=-o
 LibraryPathSwitch      :=-L
 PreprocessorSwitch     :=-D
 SourceSwitch           :=-c 
-OutputDirectory        :=C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/build-$(WorkspaceConfiguration)/bin
+OutputDirectory        :=C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/build-$(WorkspaceConfiguration)/bin
 OutputFile             :=..\build-$(WorkspaceConfiguration)\bin\$(ProjectName).exe
 Preprocessors          :=
 ObjectSwitch           :=-o 
@@ -63,9 +63,9 @@ AS       := as
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Users\rosaa\Downloads\codelite-amd64-17.0.0\codelite
-Objects0=$(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix) $(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix) $(IntermediateDirectory)/Cola.cpp$(ObjectSuffix) $(IntermediateDirectory)/Gestor.cpp$(ObjectSuffix) $(IntermediateDirectory)/Lista.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoArbol.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoLista.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix) $(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix) $(IntermediateDirectory)/Pila.cpp$(ObjectSuffix) \
-	$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+CodeLiteDir:=C:\codelite-amd64-17.0.0\codelite
+Objects0=$(IntermediateDirectory)/Pila.cpp$(ObjectSuffix) $(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix) $(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/Gestor.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix) $(IntermediateDirectory)/Lista.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoArbol.cpp$(ObjectSuffix) $(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix) $(IntermediateDirectory)/NodoLista.cpp$(ObjectSuffix) \
+	$(IntermediateDirectory)/Cola.cpp$(ObjectSuffix) 
 
 
 
@@ -96,60 +96,60 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix): NodoPila.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/NodoPila.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/NodoPila.cpp$(PreprocessSuffix): NodoPila.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoPila.cpp$(PreprocessSuffix) NodoPila.cpp
+$(IntermediateDirectory)/Pila.cpp$(ObjectSuffix): Pila.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Pila.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Pila.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Pila.cpp$(PreprocessSuffix): Pila.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Pila.cpp$(PreprocessSuffix) Pila.cpp
 
-$(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix): Arbol.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Arbol.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Arbol.cpp$(PreprocessSuffix): Arbol.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Arbol.cpp$(PreprocessSuffix) Arbol.cpp
+$(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix): Aficionados.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Aficionados.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Aficionados.cpp$(PreprocessSuffix): Aficionados.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Aficionados.cpp$(PreprocessSuffix) Aficionados.cpp
 
-$(IntermediateDirectory)/Cola.cpp$(ObjectSuffix): Cola.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Cola.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cola.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Cola.cpp$(PreprocessSuffix): Cola.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Cola.cpp$(PreprocessSuffix) Cola.cpp
+$(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix): NodoCola.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/NodoCola.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/NodoCola.cpp$(PreprocessSuffix): NodoCola.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoCola.cpp$(PreprocessSuffix) NodoCola.cpp
+
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
 
 $(IntermediateDirectory)/Gestor.cpp$(ObjectSuffix): Gestor.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Gestor.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Gestor.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Gestor.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Gestor.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Gestor.cpp$(PreprocessSuffix): Gestor.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Gestor.cpp$(PreprocessSuffix) Gestor.cpp
 
+$(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix): NodoPila.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/NodoPila.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoPila.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/NodoPila.cpp$(PreprocessSuffix): NodoPila.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoPila.cpp$(PreprocessSuffix) NodoPila.cpp
+
 $(IntermediateDirectory)/Lista.cpp$(ObjectSuffix): Lista.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Lista.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Lista.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Lista.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Lista.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Lista.cpp$(PreprocessSuffix): Lista.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Lista.cpp$(PreprocessSuffix) Lista.cpp
 
 $(IntermediateDirectory)/NodoArbol.cpp$(ObjectSuffix): NodoArbol.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/NodoArbol.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoArbol.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/NodoArbol.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoArbol.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/NodoArbol.cpp$(PreprocessSuffix): NodoArbol.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoArbol.cpp$(PreprocessSuffix) NodoArbol.cpp
 
+$(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix): Arbol.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Arbol.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Arbol.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Arbol.cpp$(PreprocessSuffix): Arbol.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Arbol.cpp$(PreprocessSuffix) Arbol.cpp
+
 $(IntermediateDirectory)/NodoLista.cpp$(ObjectSuffix): NodoLista.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/NodoLista.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoLista.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/NodoLista.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoLista.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/NodoLista.cpp$(PreprocessSuffix): NodoLista.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoLista.cpp$(PreprocessSuffix) NodoLista.cpp
 
-$(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix): NodoCola.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/NodoCola.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NodoCola.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/NodoCola.cpp$(PreprocessSuffix): NodoCola.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NodoCola.cpp$(PreprocessSuffix) NodoCola.cpp
-
-$(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix): Aficionados.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Aficionados.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Aficionados.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Aficionados.cpp$(PreprocessSuffix): Aficionados.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Aficionados.cpp$(PreprocessSuffix) Aficionados.cpp
-
-$(IntermediateDirectory)/Pila.cpp$(ObjectSuffix): Pila.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/Pila.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Pila.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Pila.cpp$(PreprocessSuffix): Pila.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Pila.cpp$(PreprocessSuffix) Pila.cpp
-
-$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp 
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/rosaa/Downloads/codelite-amd64-17.0.0/codelite/Workspace/PL1/PECLBaenaAlonsoJorge/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+$(IntermediateDirectory)/Cola.cpp$(ObjectSuffix): Cola.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/lolpi/Downloads/Sesion1m/Sesion1-main/PL1/PECLBaenaAlonsoJorge/Cola.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Cola.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Cola.cpp$(PreprocessSuffix): Cola.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Cola.cpp$(PreprocessSuffix) Cola.cpp
 
 ##
 ## Clean
